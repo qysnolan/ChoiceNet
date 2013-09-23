a214364558f3d93cdfde239d76256357f4db4c18
qysnolan <qysnolan@gmail.com>
2013-09-23 12:25:49 -0400

    add dynamic navi bar

--- choiceNet/functions.py
+++ choiceNet/functions.py
@@ -2,17 +2,75 @@ from django.shortcuts import render
 from django.core.context_processors import csrf
 
 
+class MenuObject:
+    name = None
+    url = None
+    forceSubmenu = False
+    icon = None
+
+    submenu = []
+
+    def __init__(self, name, url=None, icon=None, disabled=False):
+        self.name = name
+        self.icon = icon
+
+        self.forceSubmenu = False
+        self.submenu = []
+
+        if url is None:
+            self.forceSubmenu = True
+        else:
+            self.url = url
+
+        self.disabled = disabled
+
+    def addSubMenu(self, menu):
+        self.submenu.append(menu)
+
+    def addSubitem(self, name, url, icon=None, disabled=False):
+        self.submenu.append(MenuObject(name, url, icon, disabled))
+
+    def hasSubmenu(self):
+        return self.submenu
+
+    def canShow(self):
+        if self.forceSubmenu and self.hasSubmenu():
+            return True
+        elif not self.forceSubmenu:
+            return True
+        return False
+
+
 def render_with_user(request, template_name, context={}):
     from website.settings import DEBUG
 
     context["debugMode"] = DEBUG
 
     user = request.user
+    context["menu"] = []
 
     if user.is_authenticated():
         context["name"] = user.fullname()
         context["user"] = request.user
         context.update(csrf(request))
+
+        speedMenu = MenuObject("High Speed")
+        speedMenu.addSubitem("Normal High Speed", "home")
+        speedMenu.addSubitem("Very High Speed", "home")
+        speedMenu.addSubitem("Extreme High Speed", "home")
+        context["menu"].append(speedMenu)
+
+        priceMenu = MenuObject("Low price")
+        priceMenu.addSubitem("Normal Low price", "home")
+        priceMenu.addSubitem("Very Low price", "home")
+        priceMenu.addSubitem("Extreme Low price", "home")
+        context["menu"].append(priceMenu)
+
+        securityMenu = MenuObject("High Security")
+        securityMenu.addSubitem("Normal High Security", "home")
+        securityMenu.addSubitem("Very High Security", "home")
+        securityMenu.addSubitem("Extreme High Security", "home")
+        context["menu"].append(securityMenu)
 
         return render(request, template_name, context)
 
