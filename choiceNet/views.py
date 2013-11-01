74e1222a206f7c1cb1173dd0c1e20f38148ee3ca
qysnolan <qysnolan@gmail.com>
2013-11-01 12:00:42 -0400

    add products on home page

--- choiceNet/views.py
+++ choiceNet/views.py
@@ -1,6 +1,7 @@
 from choiceNet.functions import render_with_user
 from django.shortcuts import HttpResponse
 from choiceNet.decorators import *
+from service.models import Service
 
 
 @logout_required
@@ -15,10 +16,10 @@ def home(request):
     if "next" in request.GET:
         redirect_to = request.GET["next"]
 
-    products = []
-    import random
-    for i in range(0, 20):
-        products.append(random.randrange(1, 1000))
+    # import random
+    # for i in range(0, 20):
+    #     products.append(random.randrange(1, 1000))
+    products = Service.objects.all()
 
     return render_with_user(request, "home.html", {"redirect_to": redirect_to,
                                                    "products": products})
