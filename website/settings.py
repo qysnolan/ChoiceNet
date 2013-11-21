6314673153316915709449093ff54cb8dbf4e8f1
qysnolan <qysnolan@gmail.com>
2013-11-21 16:11:47 -0500

    open api permission

--- website/settings.py
+++ website/settings.py
@@ -176,7 +176,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.BasicAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": (
-        "rest_framework.permissions.IsAuthenticated",
+        # "rest_framework.permissions.IsAuthenticated",
     ),
     "DEFAULT_FILTER_BACKENDS": (
         "rest_framework.filters.DjangoFilterBackend",
