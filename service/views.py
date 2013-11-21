6e14aa6eb9a70b9c939bbd62dcb2b6646ac8629e
qysnolan <qysnolan@gmail.com>
2013-11-21 16:09:09 -0500

    improve welcome page

--- service/views.py
+++ service/views.py
@@ -4,7 +4,10 @@ from choiceNet.functions import render_with_user
 def ServicesList(request):
 
     post = request.POST
-    searchValue = post["searchValue"]
+    try:
+        searchValue = post["searchValue"]
+    except:
+        searchValue = ""
 
     return render_with_user(request, 'services/index.html',
                             {'searchValue': searchValue})
