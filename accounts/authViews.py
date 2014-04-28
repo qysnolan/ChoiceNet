739596a9550e40a55cfc898a33a40ea33fdd7b48
qysnolan <qysnolan@gmail.com>
2014-04-28 16:27:31 -0400

    add manual auth page

---
+++ accounts/authViews.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.contrib.auth import authenticate
+
+
+def login(request):
+
+    username_tag = "Username (email)"
+    password_tag = "Password"
+    login = False
+
+    if request.method == 'GET':
+
+        return render(request, 'accounts/auth_login.html',
+                      {"login": login, "username_tag": username_tag,
+                       "password_tag": password_tag, })
+
+    if request.method == 'POST':
+
+        username = request.POST["username"]
+        password = request.POST["password"]
+        success = False
+        user = authenticate(username=username, password=password)
+        login = True
+
+        if user is not None:
+            success = True
+
+        return render(request, 'accounts/auth_login.html',
+                      {"success": success, "login": login,
+                       "username_tag": username_tag,
+                       "password_tag": password_tag, })
