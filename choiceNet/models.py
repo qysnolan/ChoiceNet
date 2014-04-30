900ecae64dd926072a93aa1b1d93b06fca615676
qysnolan <qysnolan@gmail.com>
2014-04-30 11:27:06 -0400

    add session model

--- choiceNet/models.py
+++ choiceNet/models.py
@@ -39,3 +39,21 @@ class Balance(models.Model):
 
     def __unicode__(self):
         return u'%s' % self.user
+
+
+class Session(models.Model):
+
+    session = models.IntegerField(blank=False, default=0, null=False)
+    user = models.ForeignKey("accounts.User", blank=False, null=False,
+                             related_name='session_user')
+    start_time = models.DateTimeField(blank=False, null=False)
+    end_time = models.DateTimeField(blank=False, null=False)
+    is_login = models.BooleanField(default=False, blank=False, null=False)
+    a = models.IntegerField(blank=False, default=0, null=False)
+    q = models.IntegerField(blank=False, default=0, null=False)
+
+    class Meta:
+        ordering = ["start_time", "user"]
+
+    def __unicode__(self):
+        return u'%s' % self.user
