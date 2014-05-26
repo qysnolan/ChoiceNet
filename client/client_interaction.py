7cfd2b7896891c0382567f9a72c328b769f031b5
qysnolan <qysnolan@gmail.com>
2014-05-26 14:44:53 -0400

    change client interaction url

--- client/client_interaction.py
+++ client/client_interaction.py
@@ -5,7 +5,7 @@ import json
 from function import *
 
 ##### Global variables #####
-url_root = 'http://0.0.0.0:8008/new/client/'
+url_root = 'http://nslab.ecs.umass.edu:8000/new/client/'
 key = None
 session_id = 0
 session = 0
